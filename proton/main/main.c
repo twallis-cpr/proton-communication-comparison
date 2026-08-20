f3ee112f870cae52c77a2ad491df1e534ab3bcf6
Tom Wallis <thomas.wallis@rockwellautomation.com>
2026-08-20 16:07:07 -0400

    Add proton transmission task

--- proton/main/main.c
+++ proton/main/main.c
@@ -19,12 +19,65 @@
 #include "target_registry_ids.h"
 #include "target_registry_sizes.h"
 
+#define PROTON_TASK_PERIOD_MS 10
+
 static char *TAG = "esp32_proton_bench";
 
-#define BENCH_STR_(x) #x
-#define BENCH_STR(x)  BENCH_STR_(x)
-
 QueueHandle_t imu_queue = NULL;
+
+extern proton_registry_t g_proton_registry;
+extern proton_node_t g_target_node;
+
+void proton_task(void *arg)
+{
+    uint8_t send_buf[1024] = { 0 };
+
+    const TickType_t period = pdMS_TO_TICKS(PROTON_TASK_PERIOD_MS);
+    TickType_t last_wake = xTaskGetTickCount();
+
+    for (;;) {
+        int64_t uptime_us = esp_timer_get_time();
+        int64_t uptime_s = uptime_us / 1000000LL;
+        uint64_t uptime_ms = uptime_us / 1000LL;
+        int64_t uptime_ns = (uptime_us % 1000000LL) * 1000LL;
+
+        ImuData_t imu_data;
+		if (xQueueReceive(imu_queue, &imu_data, 0) == pdPASS) {
+            proton_signal_set_float(&g_proton_registry, PROTON_SIGNAL_ANGULAR_X_ID, imu_data.angular_vel_x);
+            proton_signal_set_float(&g_proton_registry, PROTON_SIGNAL_ANGULAR_Y_ID, imu_data.angular_vel_y);
+            proton_signal_set_float(&g_proton_registry, PROTON_SIGNAL_ANGULAR_Z_ID, imu_data.angular_vel_z);
+            proton_signal_set_float(&g_proton_registry, PROTON_SIGNAL_LINEAR_X_ID, imu_data.linear_accel_x);
+            proton_signal_set_float(&g_proton_registry, PROTON_SIGNAL_LINEAR_Y_ID, imu_data.linear_accel_y);
+            proton_signal_set_float(&g_proton_registry, PROTON_SIGNAL_LINEAR_Z_ID, imu_data.linear_accel_z);
+
+            proton_signal_set_int64(&g_proton_registry, PROTON_SIGNAL_TS_SEC_ID, uptime_s);
+            proton_signal_set_int64(&g_proton_registry, PROTON_SIGNAL_TS_NSEC_ID, uptime_ns);
+        }
+
+        size_t out_len = 0;
+        size_t num_selected_peers = 0;
+        proton_endpoint_t dest_peers[1];
+
+        proton_status_e status = proton_node_update(
+            &g_target_node,
+            uptime_ms,
+            &send_buf[sizeof(proton_udp4_header_t)],
+            sizeof(send_buf),
+            &out_len,
+            dest_peers,
+            1,
+            &num_selected_peers;
+        );
+
+        if (status == PROTON_OK) {
+            proton_udp4_header_t header;
+            proton_udp4_fill_header(&header, dest_peers[0].node_id, 0);
+            memcpy(send_buf, &header, sizeof(header));
+        }
+
+        vTaskDelayUntil(&last_wake, period);
+    }
+}
 
 void app_main(void)
 {
@@ -36,16 +89,17 @@ void app_main(void)
                  esp_err_to_name(err));
     }
 
+    g_target_node.registry = &g_proton_registry;
+
 	imu_queue = xQueueCreate(4, sizeof(ImuData_t));
 
 	if (imu_queue != NULL) {
-		//pin micro-ros task in APP_CPU to make PRO_CPU to deal with wifi:
-		// xTaskCreate(micro_ros_task,
-		// 		"uros_task",
-		// 		CONFIG_MICRO_ROS_APP_STACK,
-		// 		(void*)imu_queue,
-		// 		CONFIG_MICRO_ROS_APP_TASK_PRIO,
-		// 		NULL);
+		xTaskCreate(proton_task,
+				"proton_task",
+				CONFIG_PROTON_TASK_STACK,
+				(void*)imu_queue,
+				CONFIG_PROTON_TASK_PRIO,
+				NULL);
 
 		imu_gen_task_start((void*)imu_queue);
 	} else {
