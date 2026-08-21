5795c281a321ba850bec18abcd5393b615908752
Tom Wallis <thomas.wallis@rockwellautomation.com>
2026-08-21 15:06:23 -0400

    Move zeno-pico path to keep things in sync

--- zenoh/main/main.c
+++ zenoh/main/main.c
@@ -32,22 +32,20 @@ void app_main(void)
 {
     ESP_LOGI(TAG, "%s booting", TAG);
 
-    esp_err_t err = wifi_sta_start_and_wait(portMAX_DELAY, PROTON_NODE_MCU_ENDPOINT_0_TRANSPORT_IP);
+    esp_err_t err = wifi_sta_start_and_wait(portMAX_DELAY, NULL);
     if (err != ESP_OK) {
         ESP_LOGW(TAG, "wifi connect failed (%s), continuing without network",
                  esp_err_to_name(err));
     }
 
-    g_target_node.registry = &g_proton_registry;
-
 	imu_queue = xQueueCreate(4, sizeof(ImuData_t));
 
 	if (imu_queue != NULL) {
-		xTaskCreate(proton_task,
+		xTaskCreate(zenoh_task,
 				"zenoh_task",
-				CONFIG_PROTON_TASK_STACK,
+				CONFIG_ZENOH_TASK_STACK,
 				(void*)imu_queue,
-				CONFIG_PROTON_TASK_PRIO,
+				CONFIG_ZENOH_TASK_PRIO,
 				NULL);
 
 		imu_gen_task_start((void*)imu_queue);
